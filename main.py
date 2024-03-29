9b3f2236c58a7276b1c3c76c4ca907d3593c9785
shuvronilmondal3 <shuvronilmondal53@gmail.com>
2024-03-29 09:59:27 +0530

    Update main.py
    now pandu can open youtube

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello")
+import win32com.client
+import speech_recognition as sr
+import webbrowser
+speaker = win32com.client.Dispatch("SAPI.SpVoice")
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        try:
+            query = r.recognize_google(audio, language="en-in")
+            print(f"User said : {query}")
+            return query
+        except Exception as e:
+            return "sorry Boss i don't understand you"
+
+s=("Hello boss")
+speaker.Speak(s)
+while True:
+    print("Listening.....")
+    text = takeCommand()
+    if "open YouTube".lower() in text.lower():
+        speaker.Speak("Opening YouTube Sir")
+        webbrowser.open("https://www.youtube.com/")
+    speaker.Speak(text)
